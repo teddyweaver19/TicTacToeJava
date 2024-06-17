1939e422f141e798cbc45bbdc51c63d56447b918
teddyweaver19 <104663520+teddyweaver19@users.noreply.github.com>
2024-06-16 20:56:46 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Java Swing Practice - Tic Tac Toe
+ *
+ * @author (Theodore Weavwer)
+ * @version (a version number or a date)
+ */
+import javax.swing.JOptionPane;
+public class Main
+{
+    public static void main(String[] args) {
+    TicTacToe newGame = new TicTacToe();
+    
+}
+}
